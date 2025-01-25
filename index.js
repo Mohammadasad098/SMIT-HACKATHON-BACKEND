3c7b0b4da7eb7d8b5ccbc58f18c5e056519a0b09
Mohammadasad098 <mohammadasadiam64@gmail.com>
2025-01-25 19:37:49 +0500

    Fix: Build issues

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import serverless from "serverless-http";
 import userRoutes from "./src/routes/user.routes.js";
-import dataRoutes from "./src/routes/data.routes.js";
+import dataRoutes from "./src/routes/data.routes.js"
 import connectDB from "./src/db/index.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+
 
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", dataRoutes);
@@ -23,14 +26,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Connect to the database
+
 connectDB()
   .then(() => {
-    console.log("⚙️  MongoDB connected successfully!");
+    app.listen(PORT, () => {
+      console.log(`⚙️  Server is running at port : ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("MONGO DB connection failed !!! ", err);
   });
-
-// Export the serverless handler
-export default serverless(app);
