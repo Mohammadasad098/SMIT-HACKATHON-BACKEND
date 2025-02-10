ddb95147737c63370a704807019464a00d1be2c1
Mohammadasad098 <mohammadasadiam64@gmail.com>
2025-02-11 04:03:53 +0500

    ...

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "*",
+  origin: "https://smit-hackathon-frontend-peach.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
