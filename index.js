bfc484764ed4c0edce46fc30c749e8215e2ad31d
Mohammadasad098 <mohammadasadiam64@gmail.com>
2025-02-14 01:16:08 +0500

    ksdam

--- index.js
+++ index.js
@@ -14,11 +14,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: "https://smit-hackathon-frontend-peach.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors({ origin: ["http://localhost:5173", "https://smit-hackathon-frontend-peach.vercel.app"] }));
+
 
 app.use(express.json());
 app.use(cookieParser());
